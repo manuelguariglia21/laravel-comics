e03a0ddc40295e25a746c416adcb197531452781
Manuel Guariglia <manuelguariglia@gmail.com>
2022-01-21 15:42:09 +0100

    test header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <h1>
-    Hello World
-  </h1>
-</body>
-</html>
+@extends('layouts.header')
+
+@section('header')
+  <header>
+    <h1>Header</h1>
+  </header>
+@endsection
